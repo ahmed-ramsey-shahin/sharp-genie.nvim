7a9a149493911c458f404074b3cea8aa54baf0b5
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-03-30 21:06:31 +0200

    Skeleton updated

--- lua/sharp_genie/init.lua
+++ lua/sharp_genie/init.lua
@@ -8,7 +8,7 @@ M.setup = function(user_config)
     M.config = vim.tbl_deep_extend("force", M.config, user_config or {})
 end
 
-M.open_todo_list = function()
+M.open_sharp_genie = function()
     print("Opening your Todo List UI...")
 end
 
